97b494ef785702e0a866de56b98b6b45200f09de
mgrom <59239618+mgrom-dev@users.noreply.github.com>
2024-08-29 14:03:23 +0700

    spring initializer

---
+++ src/main/java/ru/mgrom/roadanalyzer/RoadanalyzerApplication.java
@@ -0,0 +1,13 @@
+package ru.mgrom.roadanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mgrom/roadanalyzer/RoadanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mgrom.roadanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

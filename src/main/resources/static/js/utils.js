87907218e20c37e0e203c8ff3b11d166ee0d474b
mgrom-dev <mgrom-dev@ya.ru>
2024-10-10 14:58:19 +0700

    make frontend

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -16,3 +16,31 @@ function fetchExpensesSync() {
         return []; // Возвращаем пустой массив в случае ошибки
     }
 }
+
+function fetchSpendingData(startDate, endDate) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `api/spending/full?createdAtAfter=${startDate}&createdAtBefore=${endDate}`, false);
+    xhr.send();
+
+    if (xhr.status === 200) {
+        return JSON.parse(xhr.responseText);
+    } else {
+        console.error('Ошибка загрузки данных:', xhr.statusText);
+        return []; // return empty array
+    }
+}
+
+function calculateSumByPartType(spendings, partType) {
+    return spendings
+        .filter(spending => spending.partType === partType)
+        .reduce((acc, spending) => acc + spending.amount, 0);
+}
+
+function formatCurrency(amount) {
+    return amount.toLocaleString('ru-RU', {
+        style: 'currency',
+        currency: 'RUB',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+}
